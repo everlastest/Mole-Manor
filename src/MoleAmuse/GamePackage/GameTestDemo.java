06e3d56cd02c217deaa57a1f696901cea64dec2b
lori0423 <83254714+lori0423@users.noreply.github.com>
2021-10-30 09:49:33 +0800

    备忘录模式改了命名

--- src/MoleAmuse/GamePackage/GameTestDemo.java
+++ src/MoleAmuse/GamePackage/GameTestDemo.java
@@ -8,27 +8,27 @@ public class GameTestDemo {
 
     public static void main(String[] args) {
         ScoreOriginator scoreOriginator = new ScoreOriginator();
-        RecordList dartsRecordList = new RecordList();
+        RecordList pointRecordList = new RecordList();
         GameMaker gameMaker = new GameMaker();
 
         gameMaker.playTictactoe();
         scoreOriginator.setRecord(10,"五子棋游戏");
-        dartsRecordList.add(scoreOriginator.saveRecordToMemento());
+        pointRecordList.add(scoreOriginator.saveRecordToMemento());
         System.out.println("\n当前游戏名称："+scoreOriginator.getName()+"\n当前分数为:"+scoreOriginator.getScore()+"\n");
 
         gameMaker.playCooking();
         scoreOriginator.setRecord(20,"做饭小游戏");
-        dartsRecordList.add(scoreOriginator.saveRecordToMemento());
+        pointRecordList.add(scoreOriginator.saveRecordToMemento());
         System.out.println("\n当前游戏名称："+scoreOriginator.getName()+"\n当前分数为:"+scoreOriginator.getScore()+"\n");
 
         gameMaker.playRacing();
         scoreOriginator.setRecord(30,"赛车游戏");
-        dartsRecordList.add(scoreOriginator.saveRecordToMemento());
+        pointRecordList.add(scoreOriginator.saveRecordToMemento());
         System.out.println("\n当前游戏名称："+scoreOriginator.getName()+"\n当前分数为:"+scoreOriginator.getScore()+"\n");
 
         System.out.println("\n开始打印得分备忘录内容...");
         for (int i=2;i>=0;i--) {
-            dartsRecordList.get(i);
+            pointRecordList.get(i);
         }
     }
 }
