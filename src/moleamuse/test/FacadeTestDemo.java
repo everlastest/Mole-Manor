925f208a1c4c17bcb792cae09500646455d98de6
lori0423 <83254714+lori0423@users.noreply.github.com>
2021-11-01 09:10:55 +0800

    facede应为facade

--- src/moleamuse/test/FacadeTestDemo.java
+++ src/moleamuse/test/FacadeTestDemo.java
@@ -2,7 +2,7 @@ package moleamuse.test;
 
 import moleamuse.GameMaker;
 
-public class FacedeTestDemo {
+public class FacadeTestDemo {
     public static void main(String[] args) {
         GameMaker.getInstance().playtestGame1();
         GameMaker.getInstance().playtestGame2();
